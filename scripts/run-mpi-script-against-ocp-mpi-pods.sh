5c4761e5963a18a84ae55e0abf13a763f1e49248
bchardim <bchardim@redhat.com>
2019-10-20 15:33:07 -0400

    prevent idle timeout on oc rsh

--- scripts/run-mpi-script-against-ocp-mpi-pods.sh
+++ scripts/run-mpi-script-against-ocp-mpi-pods.sh
@@ -8,11 +8,13 @@ echo "#######################################################"
 echo ""
 
 app_name=mpi
-mpi_pods=`oc get po -l app=${app_name} -o wide`
+mpi_pods=`oc get pod -l app=${app_name} -o wide`
+mpi_pods_master=`oc get pod -l app=${app_name}-master -o wide`
+mpi_pods=${mpi_pods_master}${mpi_pods}
 mpi_pods_names=`echo "${mpi_pods}" | awk 'FNR > 1 {print $1}'`
 mpi_pods_ips=`echo "${mpi_pods}" | awk 'FNR > 1 {print $6}'`
 mpi_pods_count=`echo "${mpi_pods_ips}" | wc -l`
-mpi_pod_head=`echo "${mpi_pods}" | awk 'FNR > 1 {print $1}' | head -n 1`
+mpi_pod_head=`echo "${mpi_pods_master}" | awk 'FNR > 1 {print $1}' | head -n 1`
 mpi_host_list=`echo "${mpi_pods_ips}" | tr '\n' ',' | sed 's/.$//'`
 mpi_scripts_dir='/home/sds/mpi-scripts'
 
@@ -40,5 +42,5 @@ echo ""
 
 echo
 echo "Run mpi script"
-echo "oc rsh ${mpi_pod_head} mpirun -np ${mpi_pods_count} -H ${mpi_host_list} scl enable rh-python36 \"bash -c '${mpi_scripts_dir}/$1 $2 $3'\""
-oc rsh ${mpi_pod_head} mpirun -np ${mpi_pods_count} -H ${mpi_host_list} scl enable rh-python36 "bash -c '${mpi_scripts_dir}/$1 $2 $3'"
+echo "oc rsh --request-timeout=3600 ${mpi_pod_head} mpirun -np ${mpi_pods_count} -H ${mpi_host_list} scl enable rh-python36 \"bash -c '${mpi_scripts_dir}/$1 $2 $3'\""
+oc rsh --request-timeout=3600 ${mpi_pod_head} mpirun -np ${mpi_pods_count} -H ${mpi_host_list} scl enable rh-python36 "bash -c '${mpi_scripts_dir}/$1 $2 $3'"
