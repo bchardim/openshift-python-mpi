9b4500233a1dff89e8dc6c0d3215ffa57340756b
bchardim <bchardim@redhat.com>
2019-10-28 13:05:11 -0400

    add sync_master.sh

---
+++ scripts/sync_master.sh
@@ -0,0 +1,6 @@
+notebook_dir=/home/sds/notebooks
+
+app_name=mpi
+mpi_pods_master=`oc get pod -l app=${app_name}-master -o wide`
+mpi_pods_names=`echo "${mpi_pods_master}" | awk 'FNR > 1 {print $1}'`
+oc rsync ${mpi_pods_names}:${notebook_dir}/ ./notebooks/ 
