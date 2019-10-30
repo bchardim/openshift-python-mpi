d5e043c2d4d0b9b856c9e5022c7efcba54880363
bchardim <bchardim@redhat.com>
2019-10-30 13:12:28 -0400

    use nfs just for .ipython dir

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -50,7 +50,7 @@ chmod 700 "${sds_home_dir}/notebooks/"
 echo 'Set home directory permisions and env'
 chown sds:root "${sds_home_dir}"
 chmod 750 "${sds_home_dir}"
-export HOME=${sds_home_dir} && cd ${sds_home_dir}
+export HOME=${sds_home_dir} && cd ${sds_home_dir} && ln -sf /.ipython .
 
 # Start sshd and jupyter-notebook
 if [[ $HOSTNAME =~ .*master.* ]]
