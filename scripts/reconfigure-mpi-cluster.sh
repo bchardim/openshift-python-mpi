eb390e753b9a58db43b6336ce23e9a6b74406144
bchardim <bchardim@redhat.com>
2019-10-31 12:58:18 -0400

    add scale-mpi-cluster.sh

---
+++ scripts/reconfigure-mpi-cluster.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+MASTER_IP=$1
+WORKER_IPS=$2
+NPROC=$3
+
+echo ""
+echo "#######################################################"
+echo "# Reconfigure mpi cluster                             #"
+echo "#######################################################"
+echo ""
+
+ipython3 profile create --parallel --profile=mpi
+jupyter serverextension enable --py ipyparallel
+jupyter nbextension enable --py ipyparallel
+
+cat > ~/.ipython/profile_mpi/ipcluster_config.py << 'HOSTEOF'
+c.IPClusterEngines.engine_launcher_class = 'MPIEngineSetLauncher'
+c.MPILauncher.mpi_args = ["-hostfile", "/home/sds/hosts"]
+c.MPILauncher.mpi_cmd = ['mpirun']
+c.MPIControllerLauncher.controller_args = ['--ip=${MASTER_IP}']
+HOSTEOF
+
+cat > ~/.ipython/profile_mpi/ipcontroller_config.py  << 'EOF'
+c.HubFactory.ip = '${MASTER_IP}'
+EOF
+
+> /home/sds/hosts 
+for i in $(echo "${WORKER_IPS}" | tr ',' '\n')
+do
+    echo "${i} slots=1" >> /home/sds/hosts
+done
+
+ipcluster stop --profile=mpi
+ipcluster stop
+rm ~/.ipython/profile_mpi/pid/*
+ipcluster start -n ${NPROC} --profile=mpi --debug
+
---
+++ scripts/scale-mpi-cluster.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+REP=$1
+
+echo ""
+echo "#######################################################"
+echo "# Scale mpi cluster                                   #"
+echo "#######################################################"
+echo ""
+
+oc scale dc mpi --replicas ${REP}
+oc wait dc mpi --for condition=available
+
+echo ""
+echo "#######################################################"
+echo "# Reading new pod distribution                        #"
+echo "#######################################################"
+echo ""
+
+app_name=mpi
+mpi_pods=`oc get pod -l app=${app_name} -o wide`
+mpi_pods_master=`oc get pod -l app=${app_name}-master -o wide`
+mpi_master_ip=`echo "${mpi_pods_master}" | awk 'FNR > 1 {print $6}' | head -n 1`
+mpi_pods=${mpi_pods_master}${mpi_pods}
+mpi_pods_names=`echo "${mpi_pods}" | awk 'FNR > 1 {print $1}'`
+mpi_pods_ips=`echo "${mpi_pods}" | awk 'FNR > 1 {print $6}'`
+mpi_pods_count=`echo "${mpi_pods_ips}" | wc -l`
+mpi_pod_head=`echo "${mpi_pods_master}" | awk 'FNR > 1 {print $1}' | head -n 1`
+mpi_host_list=`echo "${mpi_pods_ips}" | tr '\n' ',' | sed 's/.$//'`
+mpi_scripts_dir='/home/sds/mpi-scripts'
+
+echo "Pod list: $(echo ${mpi_pods_names} | tr '\n' ' ')"
+echo "Pod count: ${mpi_pods_count}"
+
+echo ""
+echo "#######################################################"
+echo "# Rsync mpi scripts to all pods                       #"
+echo "#######################################################"
+echo ""
+for mpi_pod in ${mpi_pods_names}; do
+  echo "${mpi_pod}"
+  oc rsync `dirname $1` ${mpi_pod}:${mpi_scripts_dir}
+  echo
+done
+
+echo ""
+echo "#######################################################"
+echo "# Run reconfigure-mpi-cluster.sh in master            #"
+echo "#######################################################"
+echo ""
+oc rsh --request-timeout=3600 ${mpi_pod_head} bash ${mpi_scripts_dir}/reconfigure-mpi-cluster.sh ${mpi_master_ip} ${mpi_pods_ips} ${mpi_pods_count} 
