7afe3d2d12a32e3b3be4ab31f25ef03327c99226
bchardim <bchardim@redhat.com>
2019-10-31 17:21:04 -0400

    increase grace period on scal operation

--- scripts/scale-mpi-cluster.sh
+++ scripts/scale-mpi-cluster.sh
@@ -10,7 +10,7 @@ echo ""
 
 oc scale dc mpi --replicas ${REP}
 oc wait dc mpi --for condition=available
-sleep 30
+sleep 60
 
 echo ""
 echo "#######################################################"
